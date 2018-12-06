7b323a5bbb26e09fb1728cb89d4d1064ad4d9ec8
Maojie Xue <maojiex@umich.edu>
2018-12-06 17:25:09 -0500

    Nega and Poses

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -1,4 +1,5 @@
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -7,18 +8,14 @@ public class testSelectionSort {
 	public void test() {
 		testPositive();
 		testNegative();
-		testZeros();
-		testDuplicates();
+		//testMixed();
+		//testDuplicates();
 	}
 
-    private void testZeros() {
-		// TODO Auto-generated method stub
-		
-	}
 
 	public testSelectionSort() {
     }
-    
+
     public void testPositive(){
         int[] arr = new int[5];
         arr[0] = 8;
@@ -27,28 +24,50 @@ public class testSelectionSort {
         arr[3] = 10;
         arr[4] = 2;
         
+        SelectionSort temp1 = new SelectionSort();
+        temp1.basicSelectionSort(arr);
+        
         int[] Sortedarr = new int[5];
         Sortedarr[0] = 2;
         Sortedarr[1] = 7;
         Sortedarr[2] = 8;
         Sortedarr[3] = 9;
         Sortedarr[4] = 10;
-        
+
+        assertArrayEquals(Sortedarr, arr);
+		
         /** add tests to check for this unit test **/
     }
-    
+
     public void testNegative(){
-        
+				int[] arr = new int[5];
+				arr[0] = -8;
+				arr[1] = -9;
+				arr[2] = -7;
+				arr[3] = -10;
+				arr[4] = -2;
+				
+				SelectionSort temp1 = new SelectionSort();
+		        temp1.basicSelectionSort(arr);
+		        
+		int[] Sortedarr = new int[5];
+        Sortedarr[0] = -10;
+        Sortedarr[1] = -9;
+        Sortedarr[2] = -8;
+        Sortedarr[3] = -7;
+        Sortedarr[4] = -2;
+
+		assertArrayEquals(Sortedarr, arr);
         /** Test data contains negative values only **/
     }
-    
+
     public void testMixed(){
-        
+
         /** Test data contains with both positive, negative and zeros **/
     }
-    
+
     public void testDuplicates(){
-        
+
         /** Test data contains duplicates **/
     }
 
