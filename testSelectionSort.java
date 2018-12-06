9d55b4ef9d040fbf1ace686ded8a1ff7dfce4644
Maojie Xue <maojiex@umich.edu>
2018-12-06 17:30:59 -0500

    mixed and duplicates

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -7,7 +7,7 @@ public class testSelectionSort {
 	public void test() {
 		testPositive();
 		testNegative();
-		testZeros();
+		testMixed();
 		testDuplicates();
 	}
 
@@ -43,12 +43,46 @@ public class testSelectionSort {
     }
     
     public void testMixed(){
+    	int[] arr = new int[5];
+        arr[0] = 8;
+        arr[1] = -9;
+        arr[2] = 7;
+        arr[3] = -10;
+        arr[4] = 0;
         
+        SelectionSort temp1 = new SelectionSort();
+        temp1.basicSelectionSort(arr);
+        
+        int[] Sortedarr = new int[5];
+        Sortedarr[0] = -10;
+        Sortedarr[1] = -9;
+        Sortedarr[2] = 0;
+        Sortedarr[3] = 7;
+        Sortedarr[4] = 8;
+        
+        assertArrayEquals(Sortedarr, arr);
         /** Test data contains with both positive, negative and zeros **/
     }
     
     public void testDuplicates(){
+    	int[] arr = new int[5];
+        arr[0] = 8;
+        arr[1] = -9;
+        arr[2] = 8;
+        arr[3] = -10;
+        arr[4] = 0;
         
+        SelectionSort temp1 = new SelectionSort();
+        temp1.basicSelectionSort(arr);
+        
+        int[] Sortedarr = new int[5];
+        Sortedarr[0] = -10;
+        Sortedarr[1] = -9;
+        Sortedarr[2] = 0;
+        Sortedarr[3] = 8;
+        Sortedarr[4] = 8;
+        
+        assertArrayEquals(Sortedarr, arr);
         /** Test data contains duplicates **/
     }
 
